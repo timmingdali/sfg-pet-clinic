d1d6bea17c8ec507e1ba67b6be9e6af12fef4f23
Tim Ming Da Li <timmingda.li@mail.utoronto.ca>
2018-09-18 23:34:15 -0400

    added PetTypeService

---
+++ pet-clinic-data/src/main/java/tim/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package tim.springframework.sfgpetclinic.services;
+
+import tim.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
